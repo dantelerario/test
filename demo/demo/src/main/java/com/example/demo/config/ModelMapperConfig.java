41894280f09f55caf195fd2bc0c826a7ff7ed69d
Dante L'Erario <dlerario@pccube.com>
2023-02-23 18:05:39 +0100

    testing propertymap

--- demo/demo/src/main/java/com/example/demo/config/ModelMapperConfig.java
+++ demo/demo/src/main/java/com/example/demo/config/ModelMapperConfig.java
@@ -1,72 +1,60 @@
 package com.example.demo.config;
 
-import java.util.stream.Collectors;
-
 import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
+
+import java.util.List;
+
+import org.modelmapper.Converter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.demo.dto.EmailDto;
 import com.example.demo.dto.PersonDto;
 import com.example.demo.entity.EmailEntity;
 import com.example.demo.entity.PersonEntity;
 
 @Configuration
 public class ModelMapperConfig {
-	
+
+	@Bean
+	public ModelMapper modelMapper() {
+
+		ModelMapper modelMapper = new ModelMapper();
+
+		modelMapper.createTypeMap(PersonEntity.class, PersonDto.class).addMapping(PersonEntity::getId, PersonDto::setId)
+				.addMapping(PersonEntity::getName, PersonDto::setName)
+				.addMapping(PersonEntity::getSurname, PersonDto::setSurname)
+				.addMapping(PersonEntity::getPhone, PersonDto::setPhone)
+				.addMapping(PersonEntity::getEmails, PersonDto::setEmails);
+
+		modelMapper.createTypeMap(EmailEntity.class, EmailDto.class).addMapping(source -> source.getPerson().getId(),
+				EmailDto::setPerson);
+
+		return modelMapper;
+
+	}
+
 //	@Bean
 //	public ModelMapper modelMapper() {
-//	    ModelMapper modelMapper = new ModelMapper();
-//	    
-//	    modelMapper.createTypeMap(PersonEntity.class, PersonDto.class)
-//	        .addMapping(PersonEntity::getId, PersonDto::setId)
-//	        .addMapping(PersonEntity::getName, PersonDto::setName)
-//	        .addMapping(PersonEntity::getSurname, PersonDto::setSurname)
-//	        .addMapping(PersonEntity::getPhone, PersonDto::setPhone)
-//	        .addMapping(source -> source.getEmails().stream()
-//	                            .map(EmailEntity::getEmail)
-//	                            .collect(Collectors.toList()), 
-//	                    PersonDto::setEmails);
-//	    
-//	    return modelMapper;
+//		return new ModelMapper();
 //	}
-
-//	   @Bean
-//	   public ModelMapper modelMapper() {
-//		   
-//	      ModelMapper modelMapper = new ModelMapper();
-//	      
-//	      modelMapper.createTypeMap(PersonEntity.class, PersonDto.class)
-//	          .addMapping(PersonEntity::getId, PersonDto::setId)
-//	          .addMapping(PersonEntity::getName, PersonDto::setName)
-//	          .addMapping(PersonEntity::getSurname, PersonDto::setSurname)
-//	          .addMapping(PersonEntity::getPhone, PersonDto::setPhone)
-//	          .addMapping(PersonEntity::getEmails, PersonDto::setEmails);
-//	          
-//	      return modelMapper;
-//	      
-//	   }
-	   
-	   @Bean
-	   public ModelMapper modelMapper() {
-		   return new  ModelMapper();
-	   }
-	   
-	   public void sourceDestinationMapping(ModelMapper modelMapper) {
-		   PropertyMap<PersonEntity, PersonDto> converter = new PropertyMap<>() {
-			   
-			   @Override
-			   protected void configure() {
-				   map().setId(source.getId());
-				   map().setName(source.getName());
-				   map().setSurname(source.getSurname());
-				   map().setPhone(source.getPhone());
-				   map().getEmails().setEmails(source.getEmails());
-			   }
-		   };
-		   
-		   modelMapper.addMappings(converter);
-		   
-	   }
-	   
+//
+//	public void sourceDestinationMapping(ModelMapper modelMapper) {
+//		PropertyMap<PersonEntity, PersonDto> converter = new PropertyMap<>() {
+//
+//			@Override
+//			protected void configure() {
+//				map().setId(source.getId());
+//				map().setName(source.getName());
+//				map().setSurname(source.getSurname());
+//				map().setPhone(source.getPhone());
+//				map().getEmails();
+//			}
+//		};
+//
+//		modelMapper.addMappings(converter);
+//
+//	}
+	
 }
